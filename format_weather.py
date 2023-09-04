1c153932b2429ab87b8cf65ae459d5339f8c450b
Volodymyr <bobkanaris@gmail.com>
2023-09-04 14:23:58 +0300

    Переробив коментарі на більш корректні

--- format_weather.py
+++ format_weather.py
@@ -2,7 +2,15 @@ from get_weather import Weather
 
 
 def format_weather(weather: Weather) -> str:
-    """The function that formats the weather data required for output"""
+    """
+    Formats weather data for display.
+
+    Args:
+        weather (Weather): The weather data to format.
+
+    Returns:
+        str: The formatted weather information as a string."""
+
     return (f"{weather.country}, {weather.city}:\n"
             f"{int(weather.temperature - 273.15)}°C, {weather.wether_type}, {weather.description}\n"
             f"Сloudiness: {weather.clouds}%, wind speed: {weather.wind_speed}m/s\n"
